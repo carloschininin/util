41b0180074659fca41efb9c7bae364695ccfc35b
Carlos Chininin <carloschininin@gmail.com>
2022-03-30 22:55:54 -0500

    [core] add class math

--- src/Math.php
+++ src/Math.php
@@ -7,31 +7,41 @@ declare(strict_types=1);
  * (c) Carlos Chininin <cio@pidia.pe>
  */
 
-namespace CarlosChininin\Util\Math;
+namespace CarlosChininin\Util;
 
-function round(?float $value, int $decimal = 2): float
-{
-    return round($value, $decimal);
-}
+use DivisionByZeroError;
 
-function number(?float $value, int $decimal = 2, bool $comma = true): string
+class Math
 {
-    if (false === is_numeric($value)) {
-        return '0';
+    public static function round(?float $value, int $decimal = 2, $mode = \PHP_ROUND_HALF_UP): float
+    {
+        return round($value, $decimal, $mode);
     }
 
-    if (true === $comma) {
-        return number_format($value, $decimal);
+    public static function number(?float $value, int $decimal = 2, bool $comma = true): string
+    {
+        if (false === is_numeric($value)) {
+            return '0.00';
+        }
+
+        if (true === $comma) {
+            return number_format($value, $decimal);
+        }
+
+        return number_format($value, $decimal, '.', '');
     }
 
-    return number_format($value, $decimal, '.', ''); //sin separador de miles ,
-}
+    public static function percentage(?float $value, ?float $total, int $decimal = 2): float
+    {
+        if (null === $total || 0.0 === $total) {
+            return 0;
+        }
 
-function percentage(?float $value, ?float $total, int $decimal = 2): float
-{
-    if (null === $total) {
+        try {
+            return round(100 * $value / $total, $decimal, \PHP_ROUND_HALF_DOWN);
+        } catch (DivisionByZeroError) {
+        }
+
         return 0;
     }
-
-    return round(100 * $value / $total);
 }
